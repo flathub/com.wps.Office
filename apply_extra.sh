ae12cd8ae78b6fb9df75b8c8a245e3a717904f72
gasinvein <gasinvein@gmail.com>
2019-07-08 14:11:06 +0300

    Fix year-prefixed icons and pdf reader binary

--- apply_extra.sh
+++ apply_extra.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -e
+shopt -s failglob
 
 mkdir -p deb-package export/share
 
@@ -10,12 +11,19 @@ mv deb-package/usr/bin/{wps,wpp,et,wpspdf} wps-office/
 mv deb-package/usr/share/{icons,applications,mime} export/share/
 rm export/share/applications/appurl.desktop
 
+YEAR_SUFFIX=2019
+
 rename "wps-office-" "com.wps.Office." export/share/{icons/hicolor/*/*,applications,mime/packages}/wps-office-*.*
+rename "wps-office${YEAR_SUFFIX}" "com.wps.Office.${YEAR_SUFFIX}" export/share/icons/hicolor/*/*/wps-office${YEAR_SUFFIX}-*.*
 
 for a in wps wpp et pdf; do
     desktop_file="export/share/applications/com.wps.Office.$a.desktop"
-    desktop-file-edit --set-key="Exec" --set-value="$a %f" "$desktop_file"
-    desktop-file-edit --set-key="Icon" --set-value="com.wps.Office.${a}main" "$desktop_file"
+    case "$a" in
+        pdf) appbin=wpspdf ;;
+        *) appbin="$a" ;;
+    esac
+    desktop-file-edit --set-key="Exec" --set-value="$appbin %f" "$desktop_file"
+    desktop-file-edit --set-key="Icon" --set-value="com.wps.Office.${YEAR_SUFFIX}-${a}main" "$desktop_file"
     desktop-file-edit --set-key="X-Flatpak-RenamedFrom" --set-value="wps-office-$a.desktop;" "$desktop_file"
 done
 sed -i 's/generic-icon name="wps-office-/icon name="com.wps.Office./g' export/share/mime/packages/com.wps.Office.*.xml
