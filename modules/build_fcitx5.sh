2f031216ca88a0bae9763419ffd44d122b417637
Weng Xuetian <wengxt@gmail.com>
2021-01-17 13:51:26 -0800

    Use FLATPAK_BUILDER_N_JOBS for parallel build

--- modules/build_fcitx5.sh
+++ modules/build_fcitx5.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-WPS_LIBRARY_DIR=$PWD/deb-package/opt/kingsoft/wps-office/office6
+WPS_LIBRARY_DIR=$PWD/wps
 
 sed -i 's/^CFG_INOTIFY=.*/CFG_INOTIFY=no/g' configure
 ./configure -opensource -confirm-license -prefix $PWD
 # Build only the necessary part
-make -C src/tools/bootstrap
-make -C src/tools/moc
-make -C src/tools/uic
-make -C src/tools/rcc
-make -C src/corelib
+make -C src/tools/bootstrap -j$FLATPAK_BUILDER_N_JOBS
+make -C src/tools/moc -j$FLATPAK_BUILDER_N_JOBS
+make -C src/tools/uic -j$FLATPAK_BUILDER_N_JOBS
+make -C src/tools/rcc -j$FLATPAK_BUILDER_N_JOBS
+make -C src/corelib -j$FLATPAK_BUILDER_N_JOBS
 
 # Replace the mismatch qconfig with wps ones.
 chmod +w src/corelib/global/qconfig.h
@@ -19,6 +19,7 @@ cd fcitx5-wps
 mkdir build
 cd build
 cmake -DQT_QMAKE_EXECUTABLE=$PWD/../../bin/qmake -DWPS_LIBRARY_DIR=$WPS_LIBRARY_DIR ..
+make -j$FLATPAK_BUILDER_N_JOBS
 make install
 
 mkdir -p /app/lib/qt
