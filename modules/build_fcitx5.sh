7459f1518928703709c189439e728990116823e6
gasinvein <gasinvein@gmail.com>
2021-01-26 21:44:05 +0300

    Simplify fcitx build script a bit

--- modules/build_fcitx5.sh
+++ modules/build_fcitx5.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+set -e
 
-WPS_LIBRARY_DIR=$PWD/wps
-
+pushd qt
 sed -i 's/^CFG_INOTIFY=.*/CFG_INOTIFY=no/g' configure
-./configure -opensource -confirm-license -prefix $PWD
+./configure \
+    -opensource \
+    -confirm-license \
+    -prefix $PWD
 # Build only the necessary part
 make -C src/tools/bootstrap -j$FLATPAK_BUILDER_N_JOBS
 make -C src/tools/moc -j$FLATPAK_BUILDER_N_JOBS
@@ -14,15 +17,13 @@ make -C src/corelib -j$FLATPAK_BUILDER_N_JOBS
 # Replace the mismatch qconfig with wps ones.
 chmod +w src/corelib/global/qconfig.h
 sed -i 's|\(#define QT_BUILD_KEY.*\) ".*"|\1 "x86_64 Linux clang full-config"|g' src/corelib/global/qconfig.h
+popd
 
-cd fcitx5-wps
-mkdir build
-cd build
-cmake -DQT_QMAKE_EXECUTABLE=$PWD/../../bin/qmake -DWPS_LIBRARY_DIR=$WPS_LIBRARY_DIR ..
-make -j$FLATPAK_BUILDER_N_JOBS
-make install
-
-mkdir -p /app/lib/qt
-mv $WPS_LIBRARY_DIR/qt/plugins/inputmethods/ /app/lib/qt
-
-
+pushd fcitx5-wps
+cmake \
+    -DQT_QMAKE_EXECUTABLE=$PWD/../qt/bin/qmake \
+    -DWPS_LIBRARY_DIR=$FLATPAK_DEST/lib \
+    -S . -B build
+make -C build -j$FLATPAK_BUILDER_N_JOBS
+make -C build install
+popd
